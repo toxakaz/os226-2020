fce5a9fa92cd5e6371597101017e57e582b8dcec
ToxaKaz <anton.a.kazancev@gmail.com>
2020-09-16 05:27:22 +0300

    global remake

--- main.c
+++ main.c
@@ -20,256 +20,199 @@ int retcode(int argc, char* argv[])
 	return code;
 }
 
-char* read_string()
-{
-	const int base_size = 32;
-
-	char* input_string = (char*)malloc(sizeof(char) * base_size);
-	int size = 0;
-
-	input_string[0] = '\0';
-	int input_string_size = base_size;
-
-	for (;;)
-	{
-		for (; size < input_string_size; size++)
-		{
-			input_string[size] = getchar();
-			if (input_string[size] == '\n')
-				break;
-		}
-		if (input_string[size] == '\n')
-			break;
-
-		char* new_string = (char*)malloc(sizeof(char) * (input_string_size << 1));
-		for (int i = 0; i < input_string_size; i++)
-			new_string[i] = input_string[i];
-
-		free(input_string);
-		input_string = new_string;
-		input_string_size <<= 1;
-	}
-
-	input_string[size++] = '\0';
-
-	return input_string;
-}
-
-char* substring(char* string, int start_pos, int len, char del_input_string)
-{
-	if (len <= 0)
-	{
-		len = start_pos;
-		for (; string[len] != '\0'; len++);
-		len -= start_pos;
-	}
-
-	char* new_string = (char*)malloc(sizeof(char) * (len + 1));
-
-	for (int i = start_pos; i < start_pos + len; i++)
-		new_string[i - start_pos] = string[i];
-	new_string[len] = '\0';
-
-	if (del_input_string)
-		free(string);
-
-	return new_string;
-}
-
-char compare(char* string_1, char* string_2, char del_input_string_1, char del_input_string_2)
+char compare(char* str_1, char* str_2)
 {
 	int i = 0;
-	for (; string_1[i] != '\0' && string_2[i] != '\0'; i++)
-		if (string_1[i] != string_2[i])
-		{
-			if (del_input_string_1)
-				free(string_1);
-			if (del_input_string_2)
-				free(string_2);
+	for (; str_1[i] != '\0' && str_2[i] != '\0'; i++)
+		if (str_1[i] != str_2[i])
 			return 0;
-		}
-	if (string_1[i] != string_2[i])
-	{
-		if (del_input_string_1)
-			free(string_1);
-		if (del_input_string_2)
-			free(string_2);
+	if (str_1[i] != str_2[i])
 		return 0;
-	}
-	if (del_input_string_1)
-		free(string_1);
-	if (del_input_string_2)
-		free(string_2);
 	return 1;
 }
 
-char*** parse(char* string, int* out_size, int** out_size_in, char del_input_string)
+struct str
 {
-	int x = 0;
-	int y = 0;
-	char*** str_arrs = 0;
-	int str_arrs_size = 0;
-	int* str_arrs_in_size = 0;
-	char end = 0;
+	int len;
+	int real_len;
+	char* str;
+};
 
-	for (;;)
+void str_add(struct str* str, char c)
+{
+	if (str->len >= str->real_len)
 	{
-		char** str_arr = 0;
-		int str_arr_size = 0;
-		char str_added = 0;
-
-		for (;;)
-		{
-			for (x = y; string[x] == ' '; x++);
-			for (y = x; string[y] != ' ' && string[y] != '\0' && string[y] != ';'; y++);
-
-			if (x == y)
-			{
-				if (string[y] == ';')
-				{
-					for (; string[y] == ';'; y++);
-					break;
-				}
-				else if (string[y] == '\0')
-				{
-					if (del_input_string)
-						free(string);
-					if (!str_added)
-					{
-						*out_size = str_arrs_size;
-						*out_size_in = str_arrs_in_size;
-						return str_arrs;
-					}
-					else
-					{
-						end = 1;
-						break;
-					}
-				}
-			}
-
-			char** new_str_arr = (char**)malloc(sizeof(char*) * (str_arr_size + 1));
-			for (int i = 0; i < str_arr_size; i++)
-				new_str_arr[i] = str_arr[i];
-			new_str_arr[str_arr_size++] = substring(string, x, y - x, 0);
-			if (str_arr != 0)
-				free(str_arr);
-			str_arr = new_str_arr;
-
-			if (!str_added)
-			{
-				int* new_str_arrs_in_size = (int*)malloc(sizeof(int) * (str_arrs_size + 1));
-				for (int i = 0; i < str_arrs_size; i++)
-					new_str_arrs_in_size[i] = str_arrs_in_size[i];
-				new_str_arrs_in_size[str_arrs_size] = 1;
-				if (str_arrs_in_size != 0)
-					free(str_arrs_in_size);
-				str_arrs_in_size = new_str_arrs_in_size;
-				str_added = 1;
-			}
-			else
-				str_arrs_in_size[str_arrs_size]++;
-
-			if (string[y] == ';')
-			{
-				for (; string[y] == ';'; y++);
-				break;
-			}
-		}
-
-		if (str_added)
-		{
-			char*** new_str_arrs = (char***)malloc(sizeof(char**) * (str_arrs_size + 1));
-			for (int i = 0; i < str_arrs_size; i++)
-				new_str_arrs[i] = str_arrs[i];
-			new_str_arrs[str_arrs_size++] = str_arr;
-			if (str_arrs != 0)
-				free(str_arrs);
-			str_arrs = new_str_arrs;
-		}
-
-		if (end)
-		{
-			*out_size = str_arrs_size;
-			*out_size_in = str_arrs_in_size;
-			return str_arrs;
-		}
+		char* new_string = (char*)malloc(sizeof(char) * (str->real_len << 1));
+		for (int i = 0; i < str->real_len; i++)
+			new_string[i] = str->str[i];
+		free(str->str);
+		str->real_len <<= 1;
+		str->str = new_string;
 	}
+
+	str->str[str->len - 1] = c;
+	str->str[str->len++] = '\0';
 }
 
-void free_parse(char*** parse, int size, int* size_in)
+struct str* init(int start_len)
+{
+	struct str* str = (struct str*)malloc(sizeof(struct str));
+	str->len = 1;
+	str->real_len = start_len;
+	str->str = (char*)malloc(sizeof(char) * start_len);
+	str->str[0] = '\0';
+
+	return str;
+}
+
+char* str_get_string(struct str* str)
+{
+	char* string = (char*)malloc(sizeof(char) * str->len);
+	for (int i = 0; i < str->len; i++)
+		string[i] = str->str[i];
+	return string;
+}
+
+void str_free(struct str* str)
+{
+	free(str->str);
+	free(str);
+}
+
+void str_reinit(struct str** str, int start_len)
+{
+	str_free(*str);
+	*str = init(start_len);
+}
+
+void command_arg_add(char*** command, int* size, char* new_arg)
+{
+	char** new_command = (char**)malloc(sizeof(char*) * (*size + 1));
+	for (int i = 0; i < *size; i++)
+		new_command[i] = (*command)[i];
+	if (*size)
+		free(*command);
+	*command = new_command;
+	(*command)[(*size)++] = new_arg;
+}
+
+void command_free(char** command, int size)
 {
 	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size_in[i]; j++)
-			free(parse[i][j]);
-		free(parse[i]);
-	}
-	free(parse);
+		free(command[i]);
+	free(command);
 }
 
-char* concat_str_arr(char** strings, int count, char* separator, char del_input_strings, char del_input_separator)
+int process(int* argc, char* argv[])
 {
-	int size = 32;
-	int counter = 0;
-	char* out_string = (char*)malloc(sizeof(char) * size);
-	for (int i = 0; i < count; i++)
-		for (char separat = 0; separat < 2; separat++)
-			for (int j = 0; ((strings[i][j] != '\0' || i == count - 1) && !separat) || (separator[j] != '\0' && separat); j++)
-			{
-				if (counter >= size)
-				{
-					char* new_out_string = (char*)malloc(sizeof(char) * (size << 1));
-					for (int g = 0; g < size; g++)
-						new_out_string[g] = out_string[g];
-					free(out_string);
-					out_string = new_out_string;
-					size <<= 1;
-				}
-				out_string[counter++] = !separat ? strings[i][j] : separator[j];
-				if (strings[i][j] == '\0' && !separat)
-					break;
-			}
+	int code = -1;
+	if (compare(argv[0], "echo"))
+		code = echo(*argc, argv);
+	else if (compare(argv[0], "retcode"))
+		code = retcode(*argc, argv);
 
-	if (del_input_strings)
-		for (int i = 0; i < count; i++)
-			free(strings);
-	if (del_input_separator)
-		free(separator);
-
-	return out_string;
+	command_free(argv, *argc);
+	*argc = 0;
+	return code;
 }
 
 int main(int argc, char* argv[])
 {
+	int automat[17][11] =
+	{
+		16, 16, 1, 16, 16, 8, 16, 0, 0, 16, 0,
+		2, 16, 16, 16, 16, 16, 16, 16, 16, 16, 1,
+		16, 16, 16, 3, 16, 16, 16, 16, 16, 16, 1,
+		16, 16, 16, 16, 4, 16, 16, 16, 16, 16, 1,
+		16, 16, 16, 16, 16, 16, 16, 5, 15, 16, 1,
+		6, 6, 6, 6, 6, 6, 6, 7, 0, 6, 2,
+		6, 6, 6, 6, 6, 6, 6, 5, 15, 6, 1,
+		6, 6, 6, 6, 6, 6, 6, 7, 0, 6, -1,
+		16, 16, 9, 16, 16, 16, 16, 16, 16, 16, 1,
+		16, 16, 16, 16, 16, 16, 10, 16, 16, 16, 1,
+		11, 16, 16, 16, 16, 16, 16, 16, 16, 16, 1,
+		16, 16, 16, 16, 12, 16, 16, 16, 16, 16, 1,
+		16, 13, 16, 16, 16, 16, 16, 16, 16, 16, 1,
+		16, 16, 14, 16, 16, 16, 16, 16, 16, 16, 1,
+		16, 16, 16, 16, 16, 16, 16, 15, 15, 16, 1,
+		16, 16, 1, 16, 16, 8, 16, 0, 0, 16, 10,
+		16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 3
+	};
+
+	int key[2 << (sizeof(char) << 3)];
+	for (int i = 0; i < 2 << (sizeof(char) << 3); i++)
+		key[i] = 9;
+	for (int i = 0; i < 9; i++)
+		key["cdehort ;"[i]] = i;
+
 	for (;;)
 	{
-		int commands_count;
-		int* command_arg_count;
-		char*** commands = parse(read_string(), &commands_count, &command_arg_count, 1);
+		int state = 0;
+		char c = getchar();
+		char error = 0;
 
-		for (int i = 0; i < commands_count; i++)
+		char** command = 0;
+		int size = 0;
+
+		struct str* str = init(8);
+		struct str* error_str = init(32);
+
+		int i = 1;
+
+		while (c != '\n')
 		{
-			if (compare(commands[i][0], "echo", 0, 0))
-				code = echo(command_arg_count[i], commands[i]);
-			else if (compare(commands[i][0], "retcode", 0, 0))
+			str_add(error_str, c);
+
+			state = automat[state][key[c]];
+			switch (automat[state][10])
 			{
-				if (command_arg_count[i] > 1)
-					printf("retcode command error\n");
-				else
-					retcode(command_arg_count[i], commands[i]);
-			}
-			else
-			{
-				char* str = concat_str_arr(commands[i], command_arg_count[i], " ", 0, 0);
-				printf("unknown command: %s\n", str);
-				free(str);
+			case 0:
+				if (size > 0)
+				{
+					str_reinit(&error_str, 32);
+					code = process(&size, command);
+				}
+				break;
+
+			case 1:
+				str_add(str, c);
+				break;
+
+			case 2:
+				command_arg_add(&command, &size, str_get_string(str));
+				str_reinit(&str, 8);
+				break;
+
+			case 3:
+				if (!error)
+					error = 1;
+				break;
+
+			case 10:
+				command_arg_add(&command, &size, str_get_string(str));
+				str_reinit(&str, 8);
+				str_reinit(&error_str, 32);
+				code = process(&size, command);
 				break;
 			}
+
+			c = getchar();
 		}
 
-		free_parse(commands, commands_count, command_arg_count);
-		free(command_arg_count);
+		if (error)
+		{
+			printf("can not process: %s", error_str->str);
+			if (size > 0)
+				command_free(command, &size);
+		}
+		else if (str->len > 1 || size > 0)
+		{
+			if (str->len > 1)
+				command_arg_add(&command, &size, str_get_string(str));
+			code = process(&size, command);
+		}
+
+		str_free(str);
+		str_free(error_str);
 	}
-	return 0;
 }
